d3d57c5a58b57c461dd1e556b3202d1fabe40048
NazarkoKril <122899742+NazarkoKril@users.noreply.github.com>
2023-09-22 15:47:25 +0300

    update

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const swiper = new Swiper(".image-slider", {
-  
-
+$(document).ready(function() {
+    const swiper = new Swiper(".image-slider", {
   loop: true,
-
- 
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
-
-  
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  
+ 
   slidesPerView: 1,
   spaceBetween: 30,
-
   speed: 1500,
-
 });
 
 document.getElementById("myForm").addEventListener("submit", function(event) {
@@ -29,9 +18,9 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
     
-    var namePattern = /^[A-Za-zА-Яа-яЁёІіЇїЄє]+$/;
-    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    var phonePattern = /^\d{10}$/;
+    var namePattern = '/^[A-Za-zА-Яа-яЁёІіЇїЄє]+$/';
+    var emailPattern = '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/';
+    var phonePattern = '/^\d{10}$/';
     
     if (!namePattern.test(firstName) || !namePattern.test(lastName)) {
         alert("Proszę wpisać poprawne imię i nazwisko.");
@@ -48,3 +37,55 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
         event.preventDefault();
     }
 });
+
+
+  var $range = $(".js-range-slider");
+var $input = $(".js-input");
+var instance;
+var min = 1100;
+var max = 500000;
+var multiplier = 6.54;
+
+$range.ionRangeSlider({
+    skin: "round",
+    type: "single",
+    min: min,
+    max: max,
+    from: 500,
+    onStart: function(data) {
+        $input.prop("value", data.from * multiplier + " PLN"); // Додаємо "PLN" до значення
+    },
+    onChange: function(data) {
+        $input.prop("value", data.from * multiplier + " PLN"); // Додаємо "PLN" до значення
+    }
+});
+
+instance = $range.data("ionRangeSlider");
+
+$input.on("input", function() {
+    var val = $(this).prop("value") / multiplier; // Розділити введене значення на множник
+
+    // validate
+    if (val < min) {
+        val = min;
+    } else if (val > max) {
+        val = max;
+    }
+
+    instance.update({
+        from: val
+    });
+
+    // Додаємо "PLN" до значення
+    $input.prop("value", val + " PLN");
+});
+
+    
+    
+    
+
+});
+
+
+
+
